b26eae8b5edd87b5eaf3a1eefbcd3ba6413d654b
danny <danixeka@gmail.com>
2019-03-21 19:17:32 +1100

    mapbox requests working

--- app.js
+++ app.js
@@ -2,18 +2,31 @@ const request = require('request');
 
 // 42.5995, 11.2918
 
-const url = 'https://api.darksky.net/forecast/8f309f032cb56d3ea822766f089e0e51/37.8267,-122.4233?units=si&lang=it';
+const DarkSkyURL = 'https://api.darksky.net/forecast/8f309f032cb56d3ea822766f089e0e51/37.8267,-122.4233?units=si&lang=it';
 
 request({
-  url: url,
+  url: DarkSkyURL,
   json: true
 }, (error, response) => {
   const current = response.body.currently;
   const daily = response.body.daily.data[0];
   // console.log(current);
-  console.log(daily);
+  // console.log(daily);
 
-  console.log(`It is currently ${ current.temperature } degrees and there is ${ current.precipProbability }% chance of rain.`);
+  // console.log(`It is currently ${ current.temperature } degrees and there is ${ current.precipProbability }% chance of rain.`);
 
-  console.log(`Oggi ci possiamo aspettare ${ daily.summary } In questo momento la temperatura e ${ current.temperature } con una probabilita di ${ current.precipProbability } percento di pioggia.`);
+  // console.log(`Oggi ci possiamo aspettare ${ daily.summary } In questo momento la temperatura e ${ current.temperature } con una probabilita di ${ current.precipProbability } percento di pioggia.`);
+})
+
+// Geocoding
+
+const MapBoxURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Magliano%20in%20Toscana.json?access_token=pk.eyJ1IjoiYWtleGluYWQiLCJhIjoiY2p0aWJ1b3d1MG53dzQzcGY1eGsyZmhlYSJ9.5M9Nprzz59r7--kUgE_BWA&limit=1"
+
+request({
+  url: MapBoxURL,
+  json: true
+}, (error, response) => {
+  const [ latitude, longitude ] = [ response.body.features[0].center[1], response.body.features[0].center[0] ];
+
+  console.log(latitude, longitude);
 })
