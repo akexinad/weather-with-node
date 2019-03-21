e91d1ddb57b3f8e655e12a307a637c4bc943b38b
danny <danixeka@gmail.com>
2019-03-21 17:30:00 +1100

    http request successful

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const request = require('request');
+
+// 42.599501, 11.291800
+
+const url = 'https://api.darksky.net/forecast/8f309f032cb56d3ea822766f089e0e51/37.8267,-122.4233';
+
+request({ url: url }, (error, response) => {
+  const data = JSON.parse(response.body);
+  console.log(data.currently);
+})
