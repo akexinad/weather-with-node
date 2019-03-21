64c3974656d72e06a289da7b1747057a6bd23ae5
danny <danixeka@gmail.com>
2019-03-21 18:12:24 +1100

    successfully pulling in data

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const request = require('request');
 
-// 42.599501, 11.291800
+// 42.5995, 11.2918
 
-const url = 'https://api.darksky.net/forecast/8f309f032cb56d3ea822766f089e0e51/37.8267,-122.4233';
+const url = 'https://api.darksky.net/forecast/8f309f032cb56d3ea822766f089e0e51/37.8267,-122.4233?units=si&lang=it';
 
 request({
   url: url,
   json: true
 }, (error, response) => {
-  console.log(response.body.currently);
+  const current = response.body.currently;
+  const daily = response.body.daily.data[0];
+  // console.log(current);
+  console.log(daily);
+
+  console.log(`It is currently ${ current.temperature } degrees and there is ${ current.precipProbability }% chance of rain.`);
+
+  console.log(`Oggi ci possiamo aspettare ${ daily.summary } In questo momento la temperatura e ${ current.temperature } con una probabilita di ${ current.precipProbability } percento di pioggia.`);
 })
